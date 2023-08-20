d790622b0d16d84acc3937719ee6db7a24afcd60
Mouad1502 <109124876+Mouad1502@users.noreply.github.com>
2023-08-20 06:55:40 +0100

    drawer added

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,44 @@
 <template>
-  <v-app>
+  <v-app id="inspire">
+    <v-navigation-drawer
+      v-model="drawer"
+      floating permanent
+      >
+        <v-list
+          density="compact"
+          nav
+        >
+          <v-list-item-title>
+            Application
+          </v-list-item-title>
+          <v-list-item-subtitle class="title">
+            Stay productive!
+          </v-list-item-subtitle>
+          <v-list-item prepend-icon="mdi-view-dashboard" title="Home" value="home" to="/"></v-list-item>
+          <v-list-item prepend-icon="mdi-forum" title="About" value="about" to="/about"></v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+
+    <v-app-bar>
+      <v-app-bar-nav-icon @click="drawer=!drawer" ></v-app-bar-nav-icon>
+
+      <v-app-bar-title>Application</v-app-bar-title>
+    </v-app-bar>
+
     <v-main>
       <router-view/>
     </v-main>
   </v-app>
 </template>
 
+<script setup>
+  import { ref } from 'vue'
+
+  const drawer = ref(null)
+</script>
+
 <script>
-
-export default {
-  name: 'App',
-
-  data: () => ({
-    //
-  }),
-}
+  export default {
+    data: () => ({ drawer: null}),
+  }
 </script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,12 @@
 <template>
-  <HelloWorld />
+  <HelloWorld/>
 </template>
 
 <script>
 import { defineComponent } from 'vue';
 
 // Components
-import HelloWorld from '../components/HelloWorld.vue';
+import HelloWorld from '../components/Home.vue';
 
 export default defineComponent({
   name: 'HomeView',
